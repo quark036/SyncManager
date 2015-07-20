8541e4b3dc496bcc04a1cc8a8b2d6299335131b5
quark036 <quark036@gmail.com>
2015-07-20 10:46:55 -0600

    stuff

---
+++ Notes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SyncManager
+{
+    class Notes
+    {
+        //fix the all on/off buttons so that they dont change invisible options
+        //figure out why the high doesnt work
+        //and now low doesnt work either
+        //also do lots of testing with the alert sound, because I'm pretty sure its bugged
+    }
+}
