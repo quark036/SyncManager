63832541de0b76df065dcbff7af8aa47d49ddc75
quark036 <quark036@gmail.com>
2015-08-17 12:09:03 -0500

    Dispose of backgroundworkers once that window is closed

--- SyncForm.cs
+++ SyncForm.cs
@@ -1044,6 +1044,12 @@ namespace SyncManager
         //stuff to do with resizing and the open windows button
         private void SyncForm_FormClosed(object sender, FormClosedEventArgs e)
         {
+            upSyncWorker.Dispose();
+            downSyncWorker.Dispose();
+            hiUpSyncWorker.Dispose();
+            hiDownSyncWorker.Dispose();
+            lowUpSyncWorker.Dispose();
+            lowDownSyncWorker.Dispose();
             if (type == 1)
                 parentForm.speakerReadyWindowOpen = false;
             else if (type == 2)
